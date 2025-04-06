75746f9ffd3ecb569a68f7d195bc93e383687e92
Anik Chandra Deb <anik.deb161@gmail.com>
2025-04-06 13:01:31 +0600

    Revert "Introduce bug in Fibonacci"
    This reverts commit 12ae47bc404841ccb2bd6641d65e5b629e55838d.

--- basic_program.cpp
+++ basic_program.cpp
@@ -10,7 +10,20 @@ int main() {
     cout << "Fibonacci Series: ";
 
     for (int i = 1; i <= n; ++i) {
-        cout<<i<<" ";
+        // Prints the first two terms.
+        if(i == 1) {
+            cout << t1 << ", ";
+            continue;
+        }
+        if(i == 2) {
+            cout << t2 << ", ";
+            continue;
+        }
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+        
+        cout << nextTerm << ", ";
     }
     return 0;
 }
