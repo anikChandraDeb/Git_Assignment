12ae47bc404841ccb2bd6641d65e5b629e55838d
Anik Chandra Deb <anik.deb161@gmail.com>
2025-04-06 13:01:04 +0600

    Introduce bug in Fibonacci

--- basic_program.cpp
+++ basic_program.cpp
@@ -10,20 +10,7 @@ int main() {
     cout << "Fibonacci Series: ";
 
     for (int i = 1; i <= n; ++i) {
-        // Prints the first two terms.
-        if(i == 1) {
-            cout << t1 << ", ";
-            continue;
-        }
-        if(i == 2) {
-            cout << t2 << ", ";
-            continue;
-        }
-        nextTerm = t1 + t2;
-        t1 = t2;
-        t2 = nextTerm;
-        
-        cout << nextTerm << ", ";
+        cout<<i<<" ";
     }
     return 0;
 }
